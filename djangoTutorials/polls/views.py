22acaa189c5ce2f22bb861a41a72f970a52706df
Sachin Singh <sachinsngh64@gmail.com>
2017-08-03 08:35:46 -0400

    polls index page works

--- djangoTutorials/polls/views.py
+++ djangoTutorials/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from django.http import *
+
+def index(request):
+    return HttpResponse("Hello, world. YOu're at the polls index")
 # Create your views here.
